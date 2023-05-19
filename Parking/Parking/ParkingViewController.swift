aee2de7c7bb8c510799de05ee9565edf7346ff60
ohdair <ohdiar@gmail.com>
2023-05-19 13:52:37 +0900

    feat: #2 NaverMap 라이브러리 추가
    - API KEY를 관리하기 위한 APIKeys.xcconfig 생성
    - pod 설치 및 라이브러리 추가
    
    close #2

--- Parking/Parking/ParkingViewController.swift
+++ Parking/Parking/ParkingViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import NMapsMap
 
 class ParkingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemIndigo
+
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
     }
 }
