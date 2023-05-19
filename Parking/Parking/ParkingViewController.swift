a2bd7c067b7badc1c6140d1d60f7f294459b1330
ohdair <ohdiar@gmail.com>
2023-05-19 11:35:45 +0900

    feat: #1 Xcode config 설정
    close #1

---
+++ Parking/Parking/ParkingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ParkingViewController.swift
+//  Parking
+//
+//  Created by 박재우 on 2023/05/19.
+//
+
+import UIKit
+
+class ParkingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
