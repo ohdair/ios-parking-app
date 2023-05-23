efcf0467cbf4ac510855068a346c1c5d1c2f6fe1
ohdair <ohdiar@gmail.com>
2023-05-23 16:36:16 +0900

    feat: #6 NaverGeocodingAPI 추가
    - URL 생성할 수 있도록 APICallable 채택
    - 문자열로 되어 있는 URL을 가진 객체 생성

---
+++ Parking/Parking/Network/NaverGeocodingAPI.swift
@@ -0,0 +1,29 @@
+//
+//  NaverGeocodingAPI.swift
+//  Parking
+//
+//  Created by 박재우 on 2023/05/23.
+//
+
+import Foundation
+
+struct NaverGeocodingAPI: APICallable {
+    var convertType: Convertable.Type = Convertable.self as! any Convertable.Type
+
+    let baseURL: String = "https://naveropenapi.apigw.ntruss.com"
+    let path: String = "/map-geocode/v2/geocode"
+    var queryItems: [String : String]?
+    var httpHeaders: [String : String]?
+    var httpMethod: HTTPMethod = .get
+
+    init(from address: String) {
+        guard let clientID = Bundle.main.object(forInfoDictionaryKey: "NMFClientId") as? String,
+              let clientSecret = Bundle.main.object(forInfoDictionaryKey: "NAVER_MAP_SECRET") as? String else {
+            return
+        }
+
+        httpHeaders = ["X-NCP-APIGW-API-KEY-ID": clientID,
+                       "X-NCP-APIGW-API-KEY": clientSecret]
+        queryItems = ["query": address]
+    }
+}
