c2f65b77a3183096432f765ea7ab22d6317e3073
ohdair <ohdiar@gmail.com>
2023-05-29 12:30:11 +0900

    refactor: 중복되는 객체 및 메서드 삭제

--- Parking/Parking/DTO/ParkingPlaceDTO/ParkingPlaceBodyDTO.swift
+++ Parking/Parking/DTO/ParkingPlaceDTO/ParkingPlaceBodyDTO.swift
@@ -70,43 +70,4 @@ extension ParkingPlaceDTO {
             }
         }
     }
-
-    func convert() -> ParkingPlaces {
-        let parkingPlaceItems = records.map { record in
-            let number = UInt(record.parkingPlaceNumber.components(separatedBy: "-").joined()) ?? 0
-            let weekDayTimeTable = TimeTable(open: record.weekdayOperationOpenTime,
-                                             close: record.weekdayOperationCloseTime)
-            let saturDayTimeTable = TimeTable(open: record.saturdayOperationOpenTime,
-                                              close: record.saturdayOperationCloseTime)
-            let holidayTimeTable = TimeTable(open: record.holidayOperationOpenTime,
-                                             close: record.holidayOperationCloseTime)
-            let operatingDayData = record.operatingDays.components(separatedBy: "+")
-            let operatingDay = OperatingDay(weekday: operatingDayData.contains("평일"),
-                                            saturday: operatingDayData.contains("토요일"),
-                                            holiday: operatingDayData.contains("공휴일"))
-            let operatingTime = OperatingTime(weekday: weekDayTimeTable,
-                                              saturday: saturDayTimeTable,
-                                              holiday: holidayTimeTable)
-            let chargeType: ChargeType = record.parkingchargeInformation == "무료" ? .free : .paid
-            let baseCharge = ChargeInformation(time: UInt(record.basicTime) ?? 0,
-                                               amount: UInt(record.basicCharge) ?? 0)
-            let additionalCharge = ChargeInformation(time: UInt(record.additionalUnitTime) ?? 0,
-                                                     amount: UInt(record.additionalUnitCharge) ?? 0)
-            let charge = ParkingPlaceCharge(type: chargeType,
-                                            base: baseCharge,
-                                            additional: additionalCharge)
-            let coordinate = Coordinate(latitude: Double(record.latitude) ?? 0,
-                                        longitude: Double(record.longitude) ?? 0)
-            return ParkingPlaceItem(number: number,
-                                    name: record.parkingPlaceName,
-                                    roadAddress: record.roadAddress,
-                                    jibunAddress: record.jibunAddress,
-                                    operatingDay: operatingDay,
-                                    operatingTime: operatingTime,
-                                    charge: charge,
-                                    coordinate: coordinate)
-        }
-
-        return ParkingPlaces(items: parkingPlaceItems)
-    }
 }
